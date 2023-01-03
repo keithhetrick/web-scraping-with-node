54aef63a8e0fc72979b239d5ed010a75a519ff52
Keith Hetrick <104343338+keithhetrick@users.noreply.github.com>
2023-01-02 18:51:41 -0600

    url image scraper added

--- url_ImageScraper/url_ImageScraper.js
+++ url_ImageScraper/url_ImageScraper.js
@@ -2,29 +2,64 @@ const puppeteer = require("puppeteer");
 const fs = require("fs");
 const path = require("path");
 
+// url to scrape
+const url = "https://www.nasa.gov/multimedia/imagegallery/iotd.html";
+
 (async () => {
   const browser = await puppeteer.launch({
-    // headless: false,
+    // open chromium browser in headless mode, comment out if not needed
+    headless: false,
   });
   const page = await browser.newPage();
+
   page.on("response", async (response) => {
     const url = response.url();
+    const status = response.status();
+
+    // if redirect is to an undefined location, the method will throw an error & continue on to the next response
+    if (status >= 300 && status <= 399) {
+      console.log(
+        "\n",
+        "Redirect from",
+        response.url(),
+        "to",
+        response.headers()["location"],
+        "\n"
+      );
+    }
+    if (status >= 500 && status <= 599) {
+      console.log(
+        "\n",
+        "Server Error",
+        response.url(),
+        "with status code",
+        response.status(),
+        "\n"
+      );
+    }
 
     if (response.request().resourceType() === "image") {
       response.buffer().then((file) => {
-        const file_Name = url.split("/").pop();
+        // if file name is too long, truncate
+        if (url.length > 255) {
+          console.log(
+            "\n",
+            "File name is too long, truncating to 255 characters...",
+            "\n"
+          );
+        }
+        // if no image is found, the method will throw an error & continue on to the next response
+        if (file.length === 0) {
+          console.log("\n", "No image found", "\n");
+        }
+
+        const file_Name = url.split("/").pop().split("?")[0];
         const file_Path = path.join(__dirname, `./images_folder/${file_Name}`);
         const writeStream = fs.createWriteStream(file_Path);
 
-        // if Response body is unavailable for redirect responses, the method will throw an error & move on to the next response
-        if (response.status() === 301) {
-          throw new Error(
-            "Response body is unavailable for redirect responses"
-          );
-        }
         writeStream.write(file);
         writeStream.on("error", (err) => {
-          console.log("Error in writing to file", err);
+          console.log("\n", "Error in writing to file", err, "\n");
         });
         writeStream.on("finish", () => {
           console.log(`Image ${file_Name} has been downloaded!`);
@@ -33,7 +68,6 @@ const path = require("path");
       });
     }
   });
-  await page.goto("https://donarturosspringhill.com/");
-  // await page.waitForTimeout(10000);
+  await page.goto(url);
   await browser.close();
 })();
